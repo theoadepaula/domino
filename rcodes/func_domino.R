83030a767f4433d463599c29585acd4e6ac33fe5
Théo de Paula <36240759+theoadepaula@users.noreply.github.com>
2019-07-19 15:08:08 -0300

    update R Project and R Code

--- rcodes/func_domino.R
+++ rcodes/func_domino.R
@@ -7,7 +7,7 @@ options(pillar.sigfigs = Inf)
 theme_update(plot.title = element_text(hjust = 0.5))
 
 
-tab_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date())){
+tab_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date()),corte=80){
   jogos_domino=read_xlsx("src/Domino.xlsm","Historico")
   jogos_domino=jogos_domino %>% 
     mutate(DataJogo=as.Date(DataJogo),
@@ -34,12 +34,15 @@ tab_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date())){
   
   #remover data.frame abaixo
   rm(jogadores_wide)
-  
+  ncorte=corte-20
   tabela_mensal=desempenho_jogadores %>% 
     filter(Ano==ano & Mes==mes) %>% group_by(Jogador) %>% 
     summarize(Jogos=n(),Vitórias=sum(Resultado=="Vitória"),Saldo_Pt=as.integer(sum(SaldoPts)),
               Buchudas=sum(Buchuda=="Sim"),Aprov=mean(Resultado=="Vitória")*100,
-              pontuacao=ifelse(Jogos<20, formatC(0,2,format="f") ,ifelse(Jogos<40,formatC((0.9+0.1*(Jogos-20)/20)*Aprov,2,format="f"),formatC(Aprov,2,format="f"))),
+              pontuacao=ifelse(Jogos<20, formatC(0,2,format="f") ,
+                               ifelse(Jogos<corte,formatC((Vitórias+0.5*(80-Jogos))/0.8, #(0.9+0.1*(Jogos-ncorte)/ncorte)*Aprov,
+                                 2,format="f"),
+                                      formatC(Aprov,2,format="f"))),
               Ano=ano,Mes=mes) %>%
     arrange(desc(pontuacao),desc(Aprov),desc(Buchudas),desc(Saldo_Pt),desc(Vitórias))
   
@@ -48,7 +51,7 @@ tab_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date())){
   tabela_mensal
 }
 
-graf_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date())){
+graf_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date()),corte=80){
   jogos_domino=read_xlsx("src/Domino.xlsm","Historico")
   jogos_domino=jogos_domino %>% 
     mutate(DataJogo=as.Date(DataJogo),
@@ -75,23 +78,27 @@ graf_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date())){
   
   #remover data.frame abaixo
   rm(jogadores_wide)
-  
+  ncorte=corte-20
   ddj=desempenho_jogadores %>% 
-    filter(Ano==year(Sys.Date()) & Mes==month(Sys.Date())) %>% 
+    filter(Ano==ano & Mes==mes) %>% 
     group_by(DataJogo,Jogador) %>% 
     summarize(Jogos=as.integer(n()),Vitorias=as.integer(sum(Resultado=="Vitória"))) %>%
     arrange(Jogador,DataJogo,desc(Vitorias)) %>%
     ungroup() %>% group_by(Jogador) %>% ungroup()%>%
     mutate(Jogador=as.factor(Jogador),JogosAc=cumsum(Jogos),VitoriasAC=cumsum(Vitorias),
            AprovAc=VitoriasAC/JogosAc*100,
-           pontuacao=ifelse(JogosAc<20,0,ifelse(JogosAc<40,(0.9+0.1*(JogosAc-20)/20)*AprovAc,AprovAc)))
+           pontuacao=ifelse(JogosAc<20,0,
+                            ifelse(Jogos<corte,
+                                   (VitoriasAC+0.5*(80-JogosAc))/0.8,
+                                   #(0.9+0.1*((Jogos-ncorte)/ncorte))*AprovAc,
+                                    AprovAc)))
   
   ddj1=ddj%>%select(-c(JogosAc,VitoriasAC,pontuacao,AprovAc))
   
   nomes_mensal= ddj %>% distinct(Jogador) %>% unlist(use.names = F)
   
-  dia_inicial=parse_date_time(paste0("1/",month(Sys.Date()),"/",year(Sys.Date())),order="dmy")
-  dia_final=parse_date_time(paste0(days_in_month(month(Sys.Date())),"/",month(Sys.Date()),"/",year(Sys.Date())),order="dmy")
+  dia_inicial=parse_date_time(paste0("1/",mes,"/",ano),order="dmy")
+  dia_final=parse_date_time(paste0(days_in_month(mes),"/",mes,"/",ano),order="dmy")
   
   djj=crossing(DataJogo=as.Date(seq(dia_inicial,to=dia_final,by="day")),Jogador=nomes_mensal)
   
@@ -103,19 +110,33 @@ graf_mes=function(mes=month(Sys.Date()),ano=year(Sys.Date())){
                                             JogosAc=as.integer(cumsum(Jogos)),
                                             VitoriasAC=as.integer(cumsum(Vitorias)),
                                             AprovAc=ifelse(JogosAc==0,0,VitoriasAC/JogosAc),
-                                            pontuacao=ifelse(JogosAc<20,0,ifelse(JogosAc<40,(0.9+0.1*(JogosAc-20)/20)*AprovAc,AprovAc)))
+                                            pontuacao=ifelse(JogosAc<20,0,ifelse(JogosAc<corte,
+                                                                                 #(VitoriasAC+0.5*(80-JogosAc))/0.8,
+                                                                                 (0.9+0.1*(JogosAc-ncorte)/ncorte)*AprovAc,
+                                                                                 AprovAc)))
   adj1$AprovAc=formatC(adj1$AprovAc,2,format="f")
   adj1$pontuacao=formatC(adj1$pontuacao,2,format="f")
 
+  if(mes==month(Sys.Date()) & ano==year(Sys.Date())){
+    adj1%>% filter(as.double(pontuacao)>0, day(DataJogo)>1 & day(DataJogo)<=day(Sys.Date()))%>% #& day(DataJogo)<=day(Sys.Date()) 
+      ggplot(aes(x=DataJogo,y=as.double(pontuacao), group=Jogador,color=Jogador))+geom_line(aes(linetype=Jogador), size=1.2)+
+      scale_y_continuous(breaks=seq(0,1,.05), labels = scales::percent)+ labs(y="Pontuação")+
+      scale_x_date(name="Dias", date_labels = "%d/%m/%Y", date_breaks = "2 day")+
+      ggtitle(paste0("Histórico de Pontuação Diária - ",toupper(month(mes, label=T, abbr=F)),"/",ano))+
+      scale_color_manual(values=lista_cores)+theme_dark()+
+      theme(plot.title = element_text(hjust = 0.5), panel.background = element_rect(fill = "gray46") ,
+            axis.text.x = element_text(angle=90))
+  }else{
+    adj1%>% filter(as.double(pontuacao)>0, day(DataJogo)>1 )%>% #& day(DataJogo)<=day(Sys.Date()) 
+      ggplot(aes(x=DataJogo,y=as.double(pontuacao), group=Jogador,color=Jogador))+geom_line(aes(linetype=Jogador), size=1.2)+
+      scale_y_continuous(breaks=seq(0,1,.05), labels = scales::percent)+ labs(y="Pontuação")+
+      scale_x_date(name="Dias", date_labels = "%d/%m/%Y", date_breaks = "2 day")+
+      ggtitle(paste0("Histórico de Pontuação Diária - ",toupper(month(mes, label=T, abbr=F)),"/",ano))+
+      scale_color_manual(values=lista_cores)+theme_dark()+
+      theme(plot.title = element_text(hjust = 0.5), panel.background = element_rect(fill = "gray46") ,
+            axis.text.x = element_text(angle=90)) 
+  }
 
-  adj1%>% filter(as.double(pontuacao)>0, day(DataJogo)>1 & day(DataJogo)<=day(Sys.Date()) )%>%
-    ggplot(aes(x=DataJogo,y=as.double(pontuacao), group=Jogador,color=Jogador))+geom_line(aes(linetype=Jogador), size=1.2)+
-    scale_y_continuous(breaks=seq(0,1,.05), labels = scales::percent)+ labs(y="Pontuação")+
-    scale_x_date(name="Dias", date_labels = "%d/%m/%Y", date_breaks = "2 day")+
-    ggtitle(paste0("Histórico de Pontuação Diária - ",toupper(month(Sys.Date(), label=T, abbr=F)),"/",year(Sys.Date())))+
-    scale_color_manual(values=lista_cores)+theme_dark()+
-    theme(plot.title = element_text(hjust = 0.5), panel.background = element_rect(fill = "gray46") ,
-          axis.text.x = element_text(angle=90))
 
 }
 
@@ -126,7 +147,7 @@ knitr::kable(tab_mes()%>%select(-c(Mes,Ano)), caption = paste("Ranking do Mês -
   row_spec(2, bold = T, color = "gray", background = "silver")%>%
   row_spec(3, bold = T, color = "#da9f65", background = "#e9c7a5") %>%
   #row_spec(which(tab_mes()[,7]<1)[1]-1, bold = T, color = "white", background = "red") %>%
-  row_spec(which(tab_mes()[,7]<1), bold = T, color = "purple", background = "green")
+  row_spec(which(tab_mes()[,7]<.10), bold = T, color = "purple", background = "green")
 
 write.csv2(tab_mes()%>%select(-c(Mes,Ano)),file=paste("csv/","ranking_mensal_",month(Sys.Date()),"_",year(Sys.Date()),".csv"))
 
